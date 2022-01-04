ab2d8463944828f67a645707ecd952e58dfeae7c
adropofliquid <74161050+adropofliquid@users.noreply.github.com>
2022-01-03 21:14:40 -0500

    Import Testing

--- src/main/java/com/dami/stockcontrol/controller/ImportController.java
+++ src/main/java/com/dami/stockcontrol/controller/ImportController.java
@@ -1,6 +1,7 @@
 package com.dami.stockcontrol.controller;
 
 import com.dami.stockcontrol.model.Product;
+import com.dami.stockcontrol.model.ProductAddInfo;
 import com.dami.stockcontrol.model.Transactions;
 import com.dami.stockcontrol.service.ProductService;
 import com.dami.stockcontrol.service.TransactionsService;
@@ -8,8 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
 
@@ -17,10 +26,39 @@ import java.util.List;
 @Controller
 public class ImportController {
 
+    private final String UPLOAD_DIR = "src/main/resources/templates/uploads/";
+
     @GetMapping("/import")
-    public String home(Model model){
+    public String importPage(Model model){
 
         return "import";
     }
 
+    @RequestMapping(value = "/import", method = RequestMethod.POST)
+    public String upload(@RequestParam("file") MultipartFile file, RedirectAttributes attributes) {
+
+        // check if file is empty
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("message", "Please select a file to upload.");
+            return "redirect:/import";
+        }
+
+        // normalize the file path
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        // save the file on the local file system
+        try {
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // return success response
+        attributes.addFlashAttribute("message", "You successfully uploaded " + fileName + '!');
+
+        return "redirect:/import";
+    }
+
 }
+
